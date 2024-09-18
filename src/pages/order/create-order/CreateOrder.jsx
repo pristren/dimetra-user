fa081ee0ce90afb8fe00dd7b60d788cadc643852
Mohammad Rafe <mrrafi5869@gmail.com>
2024-09-18 17:30:21 +0600

    user have to full fill the current step to go to next tab

--- src/pages/order/create-order/CreateOrder.jsx
+++ src/pages/order/create-order/CreateOrder.jsx
@@ -22,56 +22,55 @@ const CreateOrder = () => {
   const [dropDate, setDropDate] = useState(null);
   const [selectedWeekdays, setSelectedWeekdays] = useState([]);
   const [createOrderData, setCreateOrderData] = useState({
-  transportationData: {
-    type_of_transport: "",
-    mode_of_transportation: [],
-    transport_with: [],
-    week_days: "",
-    start_date: startDate,
-    end_date: endDate,
-    start_time: "",
-    return_time: "",
-    multiple_weekdays: [],
-    ends: "",
-  },
-  patientData: {
-    name: "",
-    surname: "",
-    date_of_birth: "",
-    area_room: "",
-    cost_center: "",
-    how_much: "",
-    special: "",
-    isolation: false,
-    patient_above_90kg: false,
-  },
-  destinationDetailsData: {
-    // pick up details
-    pick_up_name: "",
-    pick_up_address: "",
-    pick_up_city: "",
-    pick_up_country: "",
-    pick_up_employee_name: "",
+    transportationData: {
+      type_of_transport: "",
+      mode_of_transportation: [],
+      transport_with: [],
+      week_days: "",
+      start_date: startDate,
+      end_date: endDate,
+      start_time: "",
+      return_time: "",
+      multiple_weekdays: [],
+      ends: "",
+    },
+    patientData: {
+      name: "",
+      surname: "",
+      date_of_birth: "",
+      area_room: "",
+      cost_center: "",
+      how_much: "",
+      special: "",
+      isolation: false,
+      patient_above_90kg: false,
+    },
+    destinationDetailsData: {
+      pick_up_name: "",
+      pick_up_address: "",
+      pick_up_city: "",
+      pick_up_country: "",
+      pick_up_employee_name: "",
 
-    drop_off_pick_up_time: "",
-    drop_off_name: "",
-    drop_off_address: "",
-    drop_off_city: "",
-    drop_off_country: "",
-    drop_off_phone: "",
+      drop_off_pick_up_time: "",
+      drop_off_name: "",
+      drop_off_address: "",
+      drop_off_city: "",
+      drop_off_country: "",
+      drop_off_phone: "",
 
-    return_day_letter: "",
-    return_approx_time: "",
-    return_floor: "",
-  },
-  billingDetailsData: {
-    pre_name: "",
-    name: "",
-    street: "",
-    place: "",
-    contact: "",
-  },
-});
+      return_day_letter: "",
+      return_approx_time: "",
+      return_floor: "",
+    },
+    billingDetailsData: {
+      pre_name: "",
+      name: "",
+      street: "",
+      place: "",
+      contact: "",
+    },
+  });
 
   const prevCreateOrderDataRef = useRef(createOrderData);
 
@@ -106,7 +105,7 @@ const CreateOrder = () => {
     setCurrentStep(step);
   };
 
-  const StepIcon = ({ step, icon, progressValue }) => (
+  const StepIcon = ({ step, icon, progressValue, isDisabled }) => (
     <div
       className={`${
         progressValue === 100
@@ -114,8 +113,14 @@ const CreateOrder = () => {
           : currentStep === step && progressValue !== 100
           ? "bg-[#FBA63C] text-white"
           : "bg-[#DFE5ED] text-black"
-      } size-40 h-max p-3 rounded-full cursor-pointer`}
-      onClick={() => handleFormChange(step)}
+      } size-40 h-max p-3 rounded-full ${
+        isDisabled ? "cursor-not-allowed" : "cursor-pointer"
+      }`}
+      onClick={() => {
+        if (!isDisabled) {
+          handleFormChange(step);
+        }
+      }}
     >
       {icon}
     </div>
@@ -154,26 +159,35 @@ const CreateOrder = () => {
             step="transportDetails"
             icon={<Pencil />}
             progressValue={transportationProgress}
+            isDisabled={false}
           />
           <Progress value={transportationProgress} />
+
           <StepIcon
             step="patientDetails"
             icon={<User />}
             progressValue={patientProgress}
+            isDisabled={transportationProgress < 100}
           />
           <Progress value={patientProgress} />
+
           <StepIcon
             step="destinationDetails"
             icon={<Truck />}
             progressValue={destinationProgress}
+            isDisabled={patientProgress < 100}
           />
           <Progress value={destinationProgress} />
+
           <StepIcon
             step="billingDetails"
             icon={<Send />}
             progressValue={billingProgress}
+            isDisabled={destinationProgress < 100}
           />
+          <Progress value={billingProgress} />
         </div>
+
         {currentStep === "transportDetails" ? (
           <TransportationDetails {...props} />
         ) : currentStep === "patientDetails" ? (
