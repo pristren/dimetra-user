38783222bfda0aecb9ad3d87fd3d94089a0ce9a6
kamruz-zzaman <kamruz.zaman8991@gmail.com>
2024-09-30 09:49:39 +0600

    get all order new query type added

--- src/pages/order/order-details/graphql/queries/getAllOrders.gql.js
+++ src/pages/order/order-details/graphql/queries/getAllOrders.gql.js
@@ -1,27 +1,32 @@
 import { gql } from "@apollo/client";
 
 export const GET_ALL_ORDERS = gql`
-  query GET_ALL_ORDERS {
-    getAllOrders {
-      user {
-        first_name
-        last_name
+  query GET_ALL_ORDERS($queryData: CommonOrderQueryType) {
+    getAllOrders(queryData: $queryData) {
+      data {
+        user {
+          first_name
+          last_name
+        }
+        id
+        status
+        transportationData {
+          type_of_transport
+          free_dates
+        }
+        patientData {
+          name
+        }
+        destinationDetailsData {
+          pick_up_address
+          drop_off_address
+          drop_off_pick_up_date
+        }
+        createdAt
       }
-      id
-      status
-      transportationData {
-        type_of_transport
-        free_dates
-      }
-      patientData {
-        name
-      }
-      destinationDetailsData {
-        pick_up_address
-        drop_off_address
-        drop_off_pick_up_date
-      }
-      createdAt
+      total
+      totalPages
+      currentPage
     }
   }
 `;
