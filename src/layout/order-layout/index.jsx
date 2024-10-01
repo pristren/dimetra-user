72cb70635fa2ea7c4a4c08c84c4828028dd29616
Mohammad Rafe <mrrafi5869@gmail.com>
2024-10-01 08:56:36 +0600

    selected image preview. show make it responsive

--- src/layout/order-layout/index.jsx
+++ src/layout/order-layout/index.jsx
@@ -5,7 +5,7 @@ import Navbar from "@/components/common/Navbar";
 const OrderLayout = () => {
   return (
     <div className="flex w-full min-h-screen">
-      <LeftSideNav />
+      {/* <LeftSideNav /> */}
       <div className="flex-1 flex flex-col">
         <Navbar />
         <div className="flex-1 px-5 bg-[#F9FCFF] py-8 overflow-y-auto">
--- src/pages/order/send-request/Message.jsx
+++ src/pages/order/send-request/Message.jsx
@@ -11,6 +11,7 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { X } from "lucide-react";
 
 const sampleMessages = [
   {
@@ -42,8 +43,15 @@ const sampleMessages = [
     displayName: "John Doe",
   },
   {
+    userId: 1,
+    text: "Hey, kemon acho? Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero doloribus est iusto ea nihil, odit quod ullam ut culpa odio?",
+    photoURL:
+      "https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp",
+    displayName: "John Doe",
+  },
+  {
     userId: 2,
-    text: "I'm doing good, how about you?",
+    text: "I'm doing good, how about you?  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero doloribus est iusto ea nihil, odit quod ullam ut culpa odio?",
     images: [],
     photoURL:
       "https://img.freepik.com/free-photo/white-offroader-jeep-parking_114579-4007.jpg?uid=R63160567&ga=GA1.1.1996921210.1706246101&semt=ais_hybrid",
@@ -82,7 +90,7 @@ const sampleMessages = [
     userId: 2,
     text: "Great! Check out these pics!",
     images: [
-      "https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
+      "https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D",
     ],
     photoURL:
       "https://img.freepik.com/free-photo/white-offroader-jeep-parking_114579-4007.jpg?uid=R63160567&ga=GA1.1.1996921210.1706246101&semt=ais_hybrid",
@@ -96,11 +104,28 @@ const sampleMessages = [
       "https://img.freepik.com/free-photo/white-offroader-jeep-parking_114579-4007.jpg?uid=R63160567&ga=GA1.1.1996921210.1706246101&semt=ais_hybrid",
     displayName: "Jane Smith",
   },
+  {
+    userId: 2,
+    text: "Wow, these are awesome!",
+    images: [],
+    photoURL:
+      "https://img.freepik.com/free-photo/white-offroader-jeep-parking_114579-4007.jpg?uid=R63160567&ga=GA1.1.1996921210.1706246101&semt=ais_hybrid",
+    displayName: "Jane Smith",
+  },
 ];
 
 const Message = ({ messages = sampleMessages, userId = 1 }) => {
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [images, setImages] = useState(null);
+  const [selectedImages, setSelectedImages] = useState([]);
 
+  const handleImageChange = (e) => {
+    const files = Array.from(e.target.files);
+    const imagesArray = files.map((file) => URL.createObjectURL(file));
+    setSelectedImages((prevImages) => [...prevImages, ...imagesArray]);
+  };
+  const handleRemoveImage = (index) => {
+    setSelectedImages((prev) => prev.filter((_, i) => i !== index));
+  };
   const renderImages = (images) => {
     if (images.length <= 4) {
       return images.map((image, index) => (
@@ -110,7 +135,9 @@ const Message = ({ messages = sampleMessages, userId = 1 }) => {
               key={index}
               src={image}
               alt={`attachment-${index}`}
-              className={`${images?.length > 1 ? "w-24 h-24" : "col-span-2"}  object-cover rounded-md cursor-pointer`}
+              className={`${
+                images?.length > 1 ? "w-20 lg:w-24 h-20 lg:h-24" : "col-span-2"
+              }  object-cover rounded-md cursor-pointer`}
             />
           </PhotoView>
         </PhotoProvider>
@@ -125,14 +152,14 @@ const Message = ({ messages = sampleMessages, userId = 1 }) => {
                   key={index}
                   src={image}
                   alt={`attachment-${index}`}
-                  className="w-24 h-24 object-cover rounded-md cursor-pointer"
+                  className="w-20 lg:w-24 h-20 lg:h-24 object-cover rounded-md cursor-pointer"
                 />
               </PhotoView>
             </PhotoProvider>
           ))}
           <div
-            className="relative w-24 h-24 bg-gray-300 rounded-md cursor-pointer"
-            onClick={() => setSelectedImage(images)}
+            className="relative w-20 lg:w-24 h-20 lg:h-24 bg-gray-300 rounded-md cursor-pointer"
+            onClick={() => setImages(images)}
           >
             <span className="absolute inset-0 flex items-center justify-center text-white font-bold bg-black bg-opacity-50 rounded-md">
               +{images.length - 3}
@@ -170,7 +197,7 @@ const Message = ({ messages = sampleMessages, userId = 1 }) => {
               ) : null}
               <div
                 className={`bg-[#6B93A7] text-white ${
-                  msg.images ? "p-3 max-w-56" : "px-3 py-1"
+                  msg.images ? "p-3 max-w-48 lg:max-w-56" : "px-3 py-1"
                 } rounded-xl ${!isFirstMessageFromUser && "mr-12"}`}
               >
                 {msg.images && (
@@ -178,7 +205,11 @@ const Message = ({ messages = sampleMessages, userId = 1 }) => {
                     {renderImages(images)}
                   </div>
                 )}
-                <p className={`${msg?.images?.length > 0 && "mt-4"}`}>
+                <p
+                  className={`${
+                    msg?.images?.length > 0 && "mt-4"
+                  } text-xs lg:text-[16px] lg:leading-6`}
+                >
                   {msg.text}
                 </p>
               </div>
@@ -194,7 +225,7 @@ const Message = ({ messages = sampleMessages, userId = 1 }) => {
               )}
               <div
                 className={`bg-secondary text-black ${
-                  msg.images ? "p-3 max-w-56" : "px-3 py-1"
+                  msg.images ? "p-3 max-w-48 lg:max-w-56" : "px-3 py-1"
                 } rounded-xl ${!isFirstMessageFromUser && "ml-12"}`}
               >
                 {msg.images && (
@@ -202,7 +233,11 @@ const Message = ({ messages = sampleMessages, userId = 1 }) => {
                     {renderImages(images)}
                   </div>
                 )}
-                <p className={`${msg?.images?.length > 0 && "mt-4"}`}>
+                <p
+                  className={`${
+                    msg?.images?.length > 0 && "mt-4"
+                  } text-xs lg:text-[16px] lg:leading-6`}
+                >
                   {msg.text}
                 </p>
               </div>
@@ -216,7 +251,7 @@ const Message = ({ messages = sampleMessages, userId = 1 }) => {
   return (
     <div className="relative">
       <h4>Order #123456</h4>
-      <div className="bg-white border border-gray-200 rounded-md mt-3 h-[calc(100vh-10rem)]">
+      <div className="bg-white border border-gray-200 rounded-md mt-3 h-[calc(100vh-3rem)]">
         <div className="flex items-center justify-start gap-3 border-b border-gray-300 px-4 py-4">
           <DefaultAvatar className="w-10 h-10 rounded-full" />
           <div>
@@ -231,24 +266,22 @@ const Message = ({ messages = sampleMessages, userId = 1 }) => {
         <div className="px-4 py-4 h-[calc(100vh-20rem)] overflow-y-auto hide-scrollbar">
           {renderMessages()}
         </div>
-        {selectedImage && (
-          <Dialog
-            open={!!selectedImage}
-            onOpenChange={() => setSelectedImage(null)}
-          >
+
+        {images && (
+          <Dialog open={!!images} onOpenChange={() => setImages(null)}>
             <DialogTrigger asChild></DialogTrigger>
-            <DialogContent className="max-h-[98vh] overflow-y-auto">
+            <DialogContent className="max-h-[98vh] w-[90%] max-w-md overflow-y-auto">
               <DialogHeader>
                 <DialogTitle>See all images</DialogTitle>
               </DialogHeader>
 
               <div className="grid grid-cols-2 gap-4">
-                {selectedImage.map((image, index) => (
+                {images.map((image, index) => (
                   <PhotoProvider key={index}>
                     <PhotoView src={image}>
                       <img
                         src={image}
-                        className="w-full h-auto object-cover cursor-pointer"
+                        className="w-full h-auto object-cover cursor-pointer rounded-lg"
                         alt=""
                       />
                     </PhotoView>
@@ -258,25 +291,43 @@ const Message = ({ messages = sampleMessages, userId = 1 }) => {
             </DialogContent>
           </Dialog>
         )}
-        <div className="flex items-center justify-center gap-3 w-full px-4 mt-auto bg-white  pt-3">
-          <div>
-            <Label htmlFor="image" className="cursor-pointer">
-              <Attach />
-            </Label>
-            <Input
-              type="file"
-              id="image"
-              className="hidden"
-              // onChange={handleImageChange}
-            />
+        <div>
+          <div className="flex flex-wrap gap-4 mt-4 px-14">
+            {selectedImages.map((image, index) => (
+              <div key={index} className="relative">
+                <img
+                  src={image}
+                  alt={`Selected ${index}`}
+                  className="w-12 lg:w-24 h-12 lg:h-24 rounded-md"
+                />
+                <X
+                  className="absolute -top-3 -right-3 p-1 size-7 bg-white border rounded-full "
+                  onClick={() => handleRemoveImage(index)}
+                />
+              </div>
+            ))}
           </div>
+          <div className="flex items-center justify-center gap-3 w-full px-4 mt-auto bg-white  pt-3">
+            <div>
+              <Label htmlFor="image" className="cursor-pointer">
+                <Attach />
+              </Label>
+              <Input
+                type="file"
+                id="image"
+                className="hidden"
+                multiple
+                onChange={handleImageChange}
+              />
+            </div>
 
-          <div className="relative w-full border border-gray-200 rounded-md">
-            <Input
-              className="w-[95%] border-none"
-              placeholder="Type a message"
-            />
-            <Send className="absolute right-3 top-1/2 -translate-y-1/2" />
+            <div className="relative w-full border border-gray-200 rounded-md">
+              <Input
+                className="w-[95%] border-none"
+                placeholder="Type a message"
+              />
+              <Send className="absolute right-3 top-1/2 -translate-y-1/2" />
+            </div>
           </div>
         </div>
       </div>
